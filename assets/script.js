56e9941659774c7e5f591c175a51c3e447e562b8
savan <savannahwilliams243@gmail.com>
2023-10-30 11:50:08 -0700

    created empy array & made vars for form and input

---
+++ assets/script.js
@@ -0,0 +1,6 @@
+//create empy array for cities
+var cities = [];
+
+// make variables for the form and input
+var cityFormEl=document.querySelector("#city-search-form");
+var cityInputEl=document.querySelector("#city");
