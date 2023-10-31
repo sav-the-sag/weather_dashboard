a887a6aae1bd4b06d17b7c91ff9cb22e6ebc037d
savan <savannahwilliams243@gmail.com>
2023-10-30 18:50:05 -0700

    added function for search btn

--- assets/script.js
+++ assets/script.js
@@ -11,3 +11,18 @@ var forecastContainerEl = document.querySelector("#fiveday-container");
 var pastSearchButtonEl = document.querySelector("#past-search-buttons");
 
 //add function for search btn
+var formSumbitHandler = function(event){
+    event.preventDefault();
+    var city = cityInputEl.value.trim();
+    if(city){
+        getCityWeather(city);
+        get5Day(city);
+        cities.unshift({city});
+        cityInputEl.value = "";
+    } 
+    else{
+        alert("Please enter a City");
+    }
+    saveSearch();
+    pastSearch(city);
+}
