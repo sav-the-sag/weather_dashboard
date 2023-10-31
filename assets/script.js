af7350f9367e4eb6d61aa8ebd5a83ef67c87d82c
savan <savannahwilliams243@gmail.com>
2023-10-30 18:01:33 -0700

    made variables using querySelector

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,13 @@
 //create empy array for cities
 var cities = [];
 
-// make variables for the form and input
+// make variables for the form, input, containers, past search btn
 var cityFormEl=document.querySelector("#city-search-form");
 var cityInputEl=document.querySelector("#city");
+var weatherContainerEl=document.querySelector("#current-weather-container");
+var citySearchInputEl = document.querySelector("#searched-city");
+var forecastTitle = document.querySelector("#forecast");
+var forecastContainerEl = document.querySelector("#fiveday-container");
+var pastSearchButtonEl = document.querySelector("#past-search-buttons");
+
+//add function for search btn
